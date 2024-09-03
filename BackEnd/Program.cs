a71e27b5774ec9fb9388daf9713557a9eddcc732
KSDeshappriya <ksdeshappriya.official@gmail.com>
2024-09-03 19:02:19 +0530

    Get automatically mysql version

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
-    new MySqlServerVersion(new Version(8, 0, 32))));
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
 builder.Services.AddScoped<AuctionService>();
 
