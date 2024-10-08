5c5714c6896c3e8695c4b66e0f8ef36307280bfe
KSDeshappriya <ksdeshappriya.official@gmail.com>
2024-10-08 11:45:59 +0530

    EmailSender: https://api.brevo.com/v3/smtp/email

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -1,5 +1,7 @@
 using BidNest.Services;
 using Microsoft.EntityFrameworkCore;
+// using sib_api_v3_sdk.Client;
+// using EmailSender = Sendinblue.EmailSender;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +25,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// // Set your API key
+// Configuration.Default.ApiKey.Add("api-key", "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");
+
+// // Call the SendEmail method
+// EmailSender.SendEmail(
+//     senderName: "BidNest",
+//     senderEmail: "kavishen1025@gmail.com",
+//     receiverEmail: "hajel32179@scarden.com",
+//     receiverName: "bidder.UserName", // Assuming you have a UserName property in your User model
+//     subject: "You won the auction!",
+//     message: $"<html><body><h1>Congratulations! You won the auction for good.</h1></body></html>"
+// );
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
