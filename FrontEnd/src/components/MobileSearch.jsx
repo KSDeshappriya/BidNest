08f867d9a3045ecd6b902572019f10e36d10c629
KSDeshappriya <ksdeshappriya.official@gmail.com>
2024-10-11 17:45:49 +0530

    components

--- FrontEnd/src/components/MobileSearch.jsx
+++ FrontEnd/src/components/MobileSearch.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const MobileSearch = () => {
     return (
         <div className="mobile-search">
--- FrontEnd/src/components/PreLoader.jsx
+++ FrontEnd/src/components/PreLoader.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function PreLoader() {
     return (
         <div className="preloader">
